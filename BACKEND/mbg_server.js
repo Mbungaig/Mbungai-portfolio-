a3f7fd2c9b4a1e3e9b2cac87489f8362bbb87d0e
Mbungaig <mbungaigeorge95@gmail.com>
2023-06-05 10:53:55 -0700

    Backend updated

---
+++ BACKEND/mbg_server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const port = parseInt(process.env.PORT) || 8080;
+
+const express = require("express");
+const app = express();
+const { connect } = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// const { msgModel } = require("./models/models");
+
+//Middle wares
+app.use(
+  cors({
+    origin: "http://localhost/3000",
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/hello", (req, res) => {
+  res.end("Welcome to my website");
+});
+
+// Connect to the database then listen for requests on PORT
+connect(process.env.MONGODB_URI)
+  .then(() =>
+    app.listen(port, () => console.log(`Listening for request on port ${port} \nopen app at http://localhost:${port}/hello`))
+  )
+  .catch((err) => console.error(`error listening on port ${port}`, err));
